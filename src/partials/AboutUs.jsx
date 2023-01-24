886421a1b029998048bd438069821568cea62d0e
avwede <avwede@gmail.com>
2023-01-24 17:04:23 -0500

    Fix wording for About Us Section

--- src/partials/AboutUs.jsx
+++ src/partials/AboutUs.jsx
@@ -9,7 +9,7 @@ function AboutUs () {
           {/* Section header */}
           <div className="max-w-5xl mx-auto text-center">
             <h1 className="h2 mb-4">About Us</h1>
-            <p className="text-xl text-gray-600">Skyline Distribution, based in Houston, Texas, specializes in providing wholesale clients with exceptional
+            <p className="text-xl text-gray-600">Skyline Distribution is based in Houston, Texas and specializes in providing wholesale clients with exceptional
               products at competitive prices. We hold the utmost importance in ensuring that every client and customer who conducts business with us experiences
               a safe and highly satisfactory transaction. We are dedicated to ensuring that our clients receive the growth and fulfillment they were promised.</p>
           </div>
