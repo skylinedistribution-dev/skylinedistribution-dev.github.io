49f68966e58aa9f9aa3f31fbc437d30b0baffbfd
avwede <avwede@gmail.com>
2023-02-17 17:41:00 -0500

    add instagram link

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import skylineLogo from '../images/skylineLogoWhite.svg'
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer () {
   return (
@@ -7,24 +8,35 @@ function Footer () {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
 
         {/* Top area: Blocks */}
-        <div className="grid grid-cols-2 border-t border-gray-200 py-5">
+        <div className="grid grid-cols-3 border-t border-gray-200 py-5 gap-5">
 
-          {/* 1st block */}
-          <div className="flex justify-center items-center">
-            <img className="w-[250px]" src={skylineLogo} />
-          </div>
+            {/* 1st block */}
+            <div className="flex justify-center items-center">
+              <img className="w-[250px]" src={skylineLogo} />
+            </div>
 
-          {/* 2th block */}
+            {/* 2nd block */}
+            <div className="flex justify-center items-center">
+              <div className="text-center">
+                <h6 className="font-bold text-gray-800 text-sm sm:text-lg mb-2">Contact Us</h6>
+                <ul className="text-xs sm:text-md text-gray-600">
+                  <li className="mb-2">5708 N Shepherd Dr, STE-D2, Houston TX, 77091</li>
+                  <li className="mb-2">info@skyline-distribution.com</li>
+                </ul>
+              </div>
+            </div>
+
+            {/* 3rd block */}
           <div className="flex justify-center items-center">
             <div className="text-center">
-              <h6 className="font-bold text-gray-800 text-md sm:text-lg mb-2">Contact Us</h6>
-              <ul className="text-sm sm:text-md text-gray-600">
-                <li className="mb-2">Skyline Distribution</li>
-                <li className="mb-2">5708 N Shepherd Dr, STE-D2, Houston TX, 77091</li>
-                <li className="mb-2">info@skyline-distribution.com</li>
+              <h6 className="font-bold text-gray-800 text-sm sm:text-lg mb-2">Follow Us</h6>
+              <ul className="text-xs sm:text-md text-gray-600">
+                <li className="mb-2"><a href="https://www.instagram.com/skylinedistribution/">@skylinedistribution</a></li>
+                <li className="mb-2"><a href="https://www.instagram.com/skylinedistribution/"><InstagramIcon /></a></li>
               </ul>
             </div>
           </div>
+
 
         </div>
       </div>
