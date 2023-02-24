fdb2ca9257a39e4f0b75e147da1e75cfb9b68a75
avwede <avwede@gmail.com>
2023-02-24 15:34:01 -0500

    Remove address

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import skylineLogo from '../images/skylineLogoWhite.svg'
 import InstagramIcon from '@mui/icons-material/Instagram';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 function Footer () {
   return (
@@ -20,8 +21,8 @@ function Footer () {
               <div className="text-center">
                 <h6 className="font-bold text-gray-800 text-sm sm:text-lg mb-2">Contact Us</h6>
                 <ul className="text-xs sm:text-md text-gray-600">
-                  <li className="mb-2">5708 N Shepherd Dr, STE-D2, Houston TX, 77091</li>
                   <li className="mb-2">info@skyline-distribution.com</li>
+                  <MailOutlineIcon />
                 </ul>
               </div>
             </div>
