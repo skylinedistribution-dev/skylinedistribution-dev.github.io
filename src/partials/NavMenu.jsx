0296d08bf02481169c3a04f9cd94aab3da29d3fb
avwede <avwede@gmail.com>
2023-05-17 09:36:04 -0400

    Fix order of sections on navbar

--- src/partials/NavMenu.jsx
+++ src/partials/NavMenu.jsx
@@ -11,10 +11,10 @@ function NavMenu () {
         <HashLink to="#products" className="font-medium text-lg text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Products</HashLink>
       </li>
       <li>
-        <HashLink to="#testimonials" className="font-medium text-lg text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Testimonials</HashLink>
+        <HashLink to="#pricing" className="font-medium text-lg text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Pricing</HashLink>
       </li>
       <li>
-        <HashLink to="#pricing" className="font-medium text-lg text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Pricing</HashLink>
+        <HashLink to="#testimonials" className="font-medium text-lg text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Testimonials</HashLink>
       </li>
     </>
   )
